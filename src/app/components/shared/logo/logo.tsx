da6718194e2bf4ae939b8c5df7504b5d29f3b67a
Rafael da Conceição <mnraffaell@gmail.com>
2025-11-12 11:10:32 -0300

    fix: format Logo component for improved readability and consistency

--- src/app/components/shared/logo/logo.tsx
+++ src/app/components/shared/logo/logo.tsx
@@ -1,13 +1,16 @@
-import InventtoIcon from "@/assets/icon.svg"
+import InventtoIcon from '@/assets/icon.svg';
 
 export const Logo = () => {
-    return (
-        <figure className="relative flex gap-1 items-center">
-            <img src={InventtoIcon} alt="Inventto Logo" className="absolute top-[-8px]">
-            </img>
-            <figcaption className="pl-10 text-green-950 text-[32px] font-bold leading-none font-philosopher">
-                Inventto
-            </figcaption>
-        </figure>
-    )
-}
+  return (
+    <figure className="relative flex gap-1 items-center">
+      <img
+        src={InventtoIcon}
+        alt="Inventto Logo"
+        className="absolute top-[-8px]"
+      ></img>
+      <figcaption className="pl-10 text-green-950 text-[32px] font-bold leading-none font-philosopher">
+        Inventto
+      </figcaption>
+    </figure>
+  );
+};
