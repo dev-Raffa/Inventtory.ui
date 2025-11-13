136242aa8b2618496b3feb70d495583a68d03500
Rafael da Conceição <mnraffaell@gmail.com>
2025-11-13 08:10:54 -0300

    chore: update Vite configuration to use Vitest for testing with jsdom environment

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import path from 'path';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
 
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,5 +11,10 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, 'src')
     }
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    css: true
   }
 });
