9ca21bd5987f6a2d6964cc8ccf9b487d71a61a3f
Rafael da Conceição <mnraffaell@gmail.com>
2025-12-04 22:07:15 -0300

    feat: enhance react-query configuration with error handling and success messages for mutations

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -11,10 +11,168 @@ export const formatBytes = (bytes: number, decimals = 2): string => {
   const k = 1024;
   const dm = decimals < 0 ? 0 : decimals;
   const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
-
   const i = Math.floor(Math.log(bytes) / Math.log(k));
 
   return (
     Number.parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i]
   );
+};
+
+export const createImage = (url: string): Promise<HTMLImageElement> =>
+  new Promise((resolve, reject) => {
+    const image = new Image();
+
+    image.addEventListener('load', () => resolve(image));
+    image.addEventListener('error', (error) => reject(error));
+    image.setAttribute('crossOrigin', 'anonymous');
+    image.src = url;
+  });
+
+export interface PixelCrop {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+}
+
+export async function getCroppedImg(
+  imageSrc: string,
+  pixelCrop: PixelCrop
+): Promise<File | null> {
+  const image = await createImage(imageSrc);
+  const canvas = document.createElement('canvas');
+  const ctx = canvas.getContext('2d');
+
+  if (!ctx) {
+    return null;
+  }
+
+  canvas.width = pixelCrop.width;
+  canvas.height = pixelCrop.height;
+
+  ctx.drawImage(
+    image,
+    pixelCrop.x,
+    pixelCrop.y,
+    pixelCrop.width,
+    pixelCrop.height,
+    0,
+    0,
+    pixelCrop.width,
+    pixelCrop.height
+  );
+
+  return new Promise((resolve) => {
+    canvas.toBlob((blob) => {
+      if (!blob) {
+        resolve(null);
+        return;
+      }
+
+      const file = new File([blob], 'avatar-cropped.jpeg', {
+        type: 'image/jpeg'
+      });
+
+      resolve(file);
+    }, 'image/jpeg');
+  });
+}
+
+export const normalizeDocument = (value: string) => value.replace(/\D/g, '');
+
+export const formatDocument = (value: string) => {
+  const numeric = normalizeDocument(value);
+  const constrained = numeric.slice(0, 14);
+
+  if (constrained.length <= 11) {
+    return constrained
+      .replace(/(\d{3})(\d)/, '$1.$2')
+      .replace(/(\d{3})(\d)/, '$1.$2')
+      .replace(/(\d{3})(\d{1,2})/, '$1-$2')
+      .replace(/(-\d{2})\d+?$/, '$1');
+  } else {
+    return constrained
+      .replace(/^(\d{2})(\d)/, '$1.$2')
+      .replace(/^(\d{2})\.(\d{3})(\d)/, '$1.$2.$3')
+      .replace(/\.(\d{3})(\d)/, '.$1/$2')
+      .replace(/(\d{4})(\d)/, '$1-$2')
+      .replace(/(-\d{2})\d+?$/, '$1');
+  }
+};
+
+const isValidCPF = (cpf: string) => {
+  cpf = normalizeDocument(cpf);
+
+  if (cpf.length !== 11 || /^(\d)\1+$/.test(cpf)) return false;
+
+  let sum = 0,
+    remainder;
+  for (let i = 1; i <= 9; i++)
+    sum += parseInt(cpf.substring(i - 1, i)) * (11 - i);
+  remainder = (sum * 10) % 11;
+
+  if (remainder === 10 || remainder === 11) remainder = 0;
+
+  if (remainder !== parseInt(cpf.substring(9, 10))) return false;
+
+  sum = 0;
+
+  for (let i = 1; i <= 10; i++)
+    sum += parseInt(cpf.substring(i - 1, i)) * (12 - i);
+  remainder = (sum * 10) % 11;
+
+  if (remainder === 10 || remainder === 11) remainder = 0;
+
+  if (remainder !== parseInt(cpf.substring(10, 11))) return false;
+
+  return true;
+};
+
+const isValidCNPJ = (cnpj: string) => {
+  cnpj = normalizeDocument(cnpj);
+
+  if (cnpj.length !== 14 || /^(\d)\1+$/.test(cnpj)) return false;
+
+  let length = cnpj.length - 2;
+  let numbers = cnpj.substring(0, length);
+
+  const digits = cnpj.substring(length);
+
+  let sum = 0;
+  let pos = length - 7;
+
+  for (let i = length; i >= 1; i--) {
+    sum += parseInt(numbers.charAt(length - i)) * pos--;
+
+    if (pos < 2) pos = 9;
+  }
+
+  let result = sum % 11 < 2 ? 0 : 11 - (sum % 11);
+
+  if (result !== parseInt(digits.charAt(0))) return false;
+
+  length = length + 1;
+  numbers = cnpj.substring(0, length);
+  sum = 0;
+  pos = length - 7;
+
+  for (let i = length; i >= 1; i--) {
+    sum += parseInt(numbers.charAt(length - i)) * pos--;
+
+    if (pos < 2) pos = 9;
+  }
+
+  result = sum % 11 < 2 ? 0 : 11 - (sum % 11);
+
+  if (result !== parseInt(digits.charAt(1))) return false;
+
+  return true;
+};
+
+export const validateDocument = (document: string) => {
+  const cleanDoc = normalizeDocument(document);
+
+  if (cleanDoc.length <= 11) return isValidCPF(cleanDoc);
+
+  return isValidCNPJ(cleanDoc);
 };
